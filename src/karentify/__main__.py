f14f252ea24f605b4d337883f0bb02f6a9874b41
Michael Lamertz <michael.lamertz@gmail.com>
2023-09-20 01:53:19 +0200

    New entrypoint

--- src/karentify/__main__.py
+++ src/karentify/__main__.py
@@ -4,12 +4,12 @@ import argparse
 from karentify import karentify
 
 
-def main():
+def main(args):
     cmdline = argparse.ArgumentParser(description='Unleash your inner Karen on `s`')
     cmdline.add_argument('--demand-manager', '-D', action='store_true',
                          help='Demand to speak to somebody with actual authority')
     cmdline.add_argument('message', type=str, nargs='*', help='Your important message')
-    opts = cmdline.parse_args(sys.argv[1:])
+    opts = cmdline.parse_args(args)
 
     if len(opts.message) > 1:
         s = ' '.join(opts.message)
@@ -21,5 +21,10 @@ def main():
         print(karentify('and I would like to talk to your manager') + '!!!')
 
 
+def run():
+    # if this is not split up, the cli can't be tested by injecting args
+    main(sys.argv[1:])
+
+
 if __name__ == "__main__":
-    main()
+    run()
