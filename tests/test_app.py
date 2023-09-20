5a781ee87b0e7e007215eb7d40cc982f07087994
Michael Lamertz <michael.lamertz@gmail.com>
2023-09-20 10:16:36 +0200

    Better testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,25 +1,54 @@
 import pytest  # noqa: F401
 
-from karentify import karentify
+import karentify
 from karentify.__main__ import main
 
 
-def test_app(capfd):
+def test_casing(capfd):
+    karentify.tiktok = None
     main(['I', 'want', 'to', 'buy', 'a', 'damburger'])
     out, _ = capfd.readouterr()
     assert out == 'I WaNt tO BuY A DaMbUrGeR!!!\n'
 
-    karentify.tiktok = False
+    karentify.tiktok = None
     main(['i', 'want', 'to', 'buy', 'a', 'damburger'])
     out, _ = capfd.readouterr()
     assert out == 'i wAnT To bUy a dAmBuRgEr!!!\n'
 
-    karentify.tiktok = False
-    main(['--demand-manager', 'drivel', 'drivel'])
+
+def test_manager(capfd):
+    karentify.tiktok = None
+    main(['--dEmAnD-MaNaGeR', 'drivel', 'drivel'])
     out, _ = capfd.readouterr()
     assert out.lower().endswith('and i would like to talk to your manager!!!\n')
 
-    karentify.tiktok = False
+    karentify.tiktok = None
     main(['-D', 'drivel', 'drivel'])
     out, _ = capfd.readouterr()
     assert out.lower().endswith('and i would like to talk to your manager!!!\n')
+
+
+def test_acting_out(capfd):
+    karentify.tiktok = None
+    main(['--AcT-ApPrOpRiAtElY', ''])
+    out, err = capfd.readouterr()
+    assert out.startswith('[')
+    assert out.endswith(']!!!\n')
+
+    karentify.tiktok = None
+    main(['-A', ''])
+    out, _ = capfd.readouterr()
+    assert out.startswith('[')
+    assert out.endswith(']!!!\n')
+
+
+def test_punctuation(capfd):
+    karentify.tiktok = None
+    main(['test.'])
+    out, _ = capfd.readouterr()
+    assert out.lower().endswith('t!!!\n')
+
+    karentify.tiktok = None
+    main(['multiple exclamation marks, a sure sign of a diseased mind!!!'])
+    out, _ = capfd.readouterr()
+    assert out.lower().endswith('d!!!\n')
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,10 +1,10 @@
 import pytest  # noqa: F401
 
-from karentify import karentify
+import karentify
 
 
 def test_karentify():
-    karentify.tiktok = False
-    assert karentify('I want to buy a damburger') == 'I WaNt tO BuY A DaMbUrGeR'
-    karentify.tiktok = False
-    assert karentify('i want to buy a damburger') == 'i wAnT To bUy a dAmBuRgEr'
+    karentify.tiktok = None
+    assert karentify.karentify('I want to buy a damburger') == 'I WaNt tO BuY A DaMbUrGeR'
+    karentify.tiktok = None
+    assert karentify.karentify('i want to buy a damburger') == 'i wAnT To bUy a dAmBuRgEr'
